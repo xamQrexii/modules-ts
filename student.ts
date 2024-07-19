3c5f240bdc8f49ab0b685d3b095d61b938e6d5f3
xamqrexii <usama1630@gmail.com>
2024-07-19 11:51:08 +0500

    feat: create students functions

---
+++ student.ts
@@ -0,0 +1,37 @@
+import students from "./data";
+import { Student } from "./interfaces";
+
+export const getAllStudents = (): Student[] => {
+  return students;
+};
+
+export const getOnsiteStudents = (): Student[] => {
+  const result = students.filter(
+    (student: Student): boolean => student.isOnsiteAllowed
+  );
+  return result;
+};
+
+export const getStudent = (rollNo: number): Student | undefined => {
+  const foundUser = students.find(
+    (student: Student): boolean => student.rollNo === rollNo
+  );
+  return foundUser;
+};
+
+export const isStudentOnSite = (rollNo: number): boolean => {
+  const foundUser = students.find(
+    (student: Student): boolean =>
+      student.rollNo === rollNo && student.isOnsiteAllowed
+  );
+
+  if (foundUser) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+// todo: get entry test passed students list
+// create function that return entry test passed students array of objects
